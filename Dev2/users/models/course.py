839b104e1267d47934b2cd922f822d02816f1be1
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-10-27 22:03:29 -0300

    modified_courses_manyToMany_to_discipline

--- Dev2/users/models/course.py
+++ Dev2/users/models/course.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-from Dev2.users.models.discipline import Discipline
+from .discipline import Discipline
 
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
     professors = models.JSONField()  # Armazenará a lista de GUIDs como JSON
-    disciplines = models.ForeignKey(Discipline, on_delete=models.CASCADE, related_name='courses')
+    disciplines = models.ManyToManyField(Discipline, related_name='courses')
 
     def __str__(self):
         return self.name
