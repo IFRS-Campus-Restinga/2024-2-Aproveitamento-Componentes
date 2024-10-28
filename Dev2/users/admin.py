dcab94b2e279b9f7385cb04524d938ad671737a7
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-10-28 20:22:22 -0300

    modified_structure_path_courses

--- Dev2/users/admin.py
+++ Dev2/users/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 
-from .models import Servant, Student, Course, Discipline
+from .models import Servant, Student
 
 # Register your models here.
 admin.site.register(Servant)
 admin.site.register(Student)
-admin.site.register(Course)
-admin.site.register(Discipline)
