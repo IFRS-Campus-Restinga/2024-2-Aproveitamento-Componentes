6b270f1b326e3a9e9e7581129855e6efeb44a8e4
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-10-27 23:08:37 -0300

    create_course_create_view

--- Dev2/users/urls.py
+++ Dev2/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views.course_view import ListCoursesAPIView
+from .views.course_view import ListCoursesAPIView, CreateCourseAPIView
 from .views.user_list_view import ListUsersAPIView
 
 urlpatterns = [
     path('list/', ListUsersAPIView.as_view(), name='list_users'),
     path('listCourse/', ListCoursesAPIView.as_view(), name='list_courses'),
+    path('createCourse/', CreateCourseAPIView.as_view(), name='create_courses'),
 ]
--- Dev2/users/views/course_view.py
+++ Dev2/users/views/course_view.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from django.db.models import Q
 from ..models import Course, Discipline
 from ..serializers.CourseSerializer import CourseSerializer
@@ -28,3 +29,31 @@ class ListCoursesAPIView(APIView):
         courses_serialized = CourseSerializer(courses, many=True)
 
         return Response({'courses': courses_serialized.data})
+
+
+class CreateCourseAPIView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        # Serializa os dados recebidos para validar e criar o novo curso
+        serializer = CourseSerializer(data=request.data)
+
+        # Verifica se os dados são válidos
+        if serializer.is_valid():
+            # Cria o curso sem disciplinas para adicioná-las posteriormente
+            course = serializer.save()
+
+            # Adiciona as disciplinas ao curso, caso sejam fornecidas
+            disciplines_data = request.data.get('disciplines')
+            if disciplines_data:
+                # Buscando as disciplinas pelo ID
+                disciplines = Discipline.objects.filter(id__in=[d['id'] for d in disciplines_data])
+                course.disciplines.set(disciplines)
+
+            # Serializa novamente para retornar os dados do curso criado
+            course_serialized = CourseSerializer(course)
+
+            # Retorna a resposta com status 201 (Created)
+            return Response(course_serialized.data, status=status.HTTP_201_CREATED)
+
+        # Caso os dados não sejam válidos, retorna os erros de validação
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
