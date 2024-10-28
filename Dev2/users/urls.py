de25b84e9b6856377eba52ecc19514ef801abb8d
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-10-28 19:19:37 -0300

    create_course_update_view

--- Dev2/users/urls.py
+++ Dev2/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views.course_view import ListCoursesAPIView, CreateCourseAPIView, RetrieveCourseAPIView
+from .views.course_view import ListCoursesAPIView, CreateCourseAPIView, RetrieveCourseAPIView,UpdateCourseAPIView
 from .views.user_list_view import ListUsersAPIView
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('course/list', ListCoursesAPIView.as_view(), name='list_courses'),
     path('course/create', CreateCourseAPIView.as_view(), name='create_course'),
     path('course/read/<int:course_id>', RetrieveCourseAPIView.as_view(), name='read_course'),
+    path('course/update/<int:course_id>/', UpdateCourseAPIView.as_view(), name='update_course'),
+
 ]
