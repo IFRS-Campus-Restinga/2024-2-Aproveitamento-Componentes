7c8361440cb3b9ba07f28fbb6383cd7de7294845
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-10-27 22:02:35 -0300

    add_entities_to_admin

--- Dev2/users/admin.py
+++ Dev2/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Servant, Student, Course, Discipline
+
 # Register your models here.
+admin.site.register(Servant)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Discipline)
--- Dev2/users/models/__init__.py
+++ Dev2/users/models/__init__.py
@@ -1,3 +1,5 @@
 from .usuario import *
 from .student import *
 from .users import Student, Servant
+from .course import *
+from .discipline import *
