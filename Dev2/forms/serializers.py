c596b5d4d41bc0dc6983b696f3cb84a46277b315
Eduardo M Comerlato <eduardomc94@gmail.com>
2024-10-29 22:48:41 -0300

    Ajuste na tela de criar forms

--- Dev2/forms/serializers.py
+++ Dev2/forms/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
-from .models import Step, Attachment, RecognitionOfPriorLearning, KnowledgeCertification
-
+from .models import Step, Attachment, RecognitionOfPriorLearning, KnowledgeCertification, RequestStatus
 
 class StepSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,38 +10,53 @@ class StepSerializer(serializers.ModelSerializer):
             'description', 'initial_step_date', 'final_step_date'
         ]
 
-
 class AttachmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attachment
         fields = ['id', 'name', 'type', 'size', 'file']
 
-
 class RecognitionOfPriorLearningSerializer(serializers.ModelSerializer):
     attachments = AttachmentSerializer(many=True, read_only=True)
-    status = serializers.CharField(source='get_status_display')
+    status_display = serializers.CharField(source='get_status_display', read_only=True)
     discipline_name = serializers.CharField(source='discipline.name', read_only=True)
 
     class Meta:
         model = RecognitionOfPriorLearning
         fields = [
             'id', 'course_workload', 'course_studied_workload', 'test_score', 'notice', 'discipline',
-            'discipline_name', 'create_date', 'status', 'servant_feedback', 'servant_analysis_date',
+            'discipline_name', 'create_date', 'status_display', 'servant_feedback', 'servant_analysis_date',
             'professor_feedback', 'professor_analysis_date', 'coordinator_feedback', 'coordinator_analysis_date',
             'attachments'
         ]
 
+    def create(self, validated_data):
+        # Extraindo os attachments, se houver
+        attachments_data = validated_data.pop('attachments', [])
+        requisition = RecognitionOfPriorLearning.objects.create(**validated_data)
+        # Se attachments existirem, adicione-os
+        for attachment in attachments_data:
+            Attachment.objects.create(requisition=requisition, **attachment)  # Certifique-se de que a relação está definida
+        return requisition
 
 class KnowledgeCertificationSerializer(serializers.ModelSerializer):
     attachments = AttachmentSerializer(many=True, read_only=True)
-    status = serializers.CharField(source='get_status_display')
+    status_display = serializers.CharField(source='get_status_display', read_only=True)
     discipline_name = serializers.CharField(source='discipline.name', read_only=True)
 
     class Meta:
         model = KnowledgeCertification
         fields = [
             'id', 'previous_knowledge', 'scheduling_date', 'test_score', 'notice', 'discipline',
-            'discipline_name', 'create_date', 'status', 'servant_feedback', 'servant_analysis_date',
+            'discipline_name', 'create_date', 'status_display', 'servant_feedback', 'servant_analysis_date',
             'professor_feedback', 'professor_analysis_date', 'coordinator_feedback', 'coordinator_analysis_date',
             'attachments'
         ]
+
+    def create(self, validated_data):
+        # Extraindo os attachments, se houver
+        attachments_data = validated_data.pop('attachments', [])
+        certification = KnowledgeCertification.objects.create(**validated_data)
+        # Se attachments existirem, adicione-os
+        for attachment in attachments_data:
+            Attachment.objects.create(certification=certification, **attachment)  # Certifique-se de que a relação está definida
+        return certification
--- frontend/src/app/(logged)/requests/requestForm/page.js
+++ frontend/src/app/(logged)/requests/requestForm/page.js
@@ -1,178 +1,186 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import {useState} from "react";
 import styles from "./requestForm.module.css";
-import { Button } from "@/components/Button/button";
-import { baseURL } from "@/libs/api";
+import {Button} from "@/components/Button/button";
+import {baseURL} from "@/libs/api";
 
 const CertificationRequestForm = () => {
-  const [requestType, setRequestType] = useState("");
-  // const [attachments, setAttachments] = useState([]); // Removido
-  const [previousKnowledge, setPreviousKnowledge] = useState("");
-  const [courseWorkload, setCourseWorkload] = useState("");
-  const [selectedCourse, setSelectedCourse] = useState(""); // Curso selecionado
-  const [disciplineId, setDisciplineId] = useState(""); // ID da disciplina que será enviado
+    const [requestType, setRequestType] = useState("");
+    const [previousKnowledge, setPreviousKnowledge] = useState("");
+    const [courseWorkload, setCourseWorkload] = useState("");
+    const [courseStudiedWorkload, setCourseStudiedWorkload] = useState("");
+    const [selectedCourse, setSelectedCourse] = useState("");
+    const [disciplineId, setDisciplineId] = useState("");
+    const [status, setStatus] = useState("CR"); // Mantenha o estado para status
 
-  // Dados mockados de cursos e disciplinas
-  const courses = [
-    { id: 1, name: "Curso de Engenharia" },
-    { id: 2, name: "Curso de Medicina" },
-    { id: 3, name: "Curso de Direito" },
-  ];
+    const courses = [
+        {id: 1, name: "Curso de Engenharia"},
+        {id: 2, name: "Curso de Medicina"},
+        {id: 3, name: "Curso de Direito"},
+    ];
 
-  const disciplinesData = {
-    1: [
-      { id: 101, name: "Matemática" },
-      { id: 102, name: "Física" },
-    ],
-    2: [
-      { id: 201, name: "Anatomia" },
-      { id: 202, name: "Fisiologia" },
-    ],
-    3: [
-      { id: 301, name: "Teoria do Direito" },
-      { id: 302, name: "Direito Civil" },
-    ],
-  };
+    const disciplinesData = {
+        1: [
+            {id: 'fc40c88d-65ae-41ca-bd19-89075f9b4ea3', name: "Matemática"},
+            {id: 'fc40c88d-65ae-41ca-bd19-89075f9b4ea3', name: "Física"},
+        ],
+        2: [
+            {id: 'fc40c88d-65ae-41ca-bd19-89075f9b4ea3', name: "Anatomia"},
+            {id: 'fc40c88d-65ae-41ca-bd19-89075f9b4ea3', name: "Fisiologia"},
+        ],
+        3: [
+            {id: 'fc40c88d-65ae-41ca-bd19-89075f9b4ea3', name: "Teoria do Direito"},
+            {id: 'fc40c88d-65ae-41ca-bd19-89075f9b4ea3', name: "Direito Civil"},
+        ],
+    };
 
-  // Estado para armazenar as disciplinas com base no curso selecionado
-  const [disciplines, setDisciplines] = useState([]);
+    const [disciplines, setDisciplines] = useState([]);
 
-  const handleCourseChange = (e) => {
-    const courseId = e.target.value;
-    setSelectedCourse(courseId);
-    setDisciplineId(""); // Reseta a disciplina ao mudar o curso
-    setDisciplines(disciplinesData[courseId] || []); // Atualiza as disciplinas com base no curso
-  };
+    const handleCourseChange = (e) => {
+        const courseId = e.target.value;
+        setSelectedCourse(courseId);
+        setDisciplineId(""); // Reseta a disciplina ao mudar o curso
+        setDisciplines(disciplinesData[courseId] || []); // Atualiza as disciplinas com base no curso
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Previne o comportamento padrão do formulário
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    // Crie um objeto FormData para enviar arquivos e outros dados
-    const formData = new FormData();
-    formData.append("discipline", disciplineId);
+        // Crie um objeto FormData para enviar arquivos e outros dados
+        const formData = new FormData();
+        formData.append("discipline", disciplineId);
+        formData.append("notice", "1f7755ade0b341299ee00c46a12dc467");
+        formData.append("course_workload", courseWorkload);
+        formData.append("course_studied_workload", courseStudiedWorkload);
+        formData.append("status", status);
 
-    // Removido: Anexos não serão enviados
+        console.log("Status sendo enviado:", status); // Adicione esta linha
 
-    if (requestType === "certificacao") {
-      formData.append("requestType", "certificacao");
-      formData.append("previous_knowledge", previousKnowledge);
-      formData.append("course_workload", courseWorkload);
-    } else if (requestType === "aproveitamento") {
-      formData.append("requestType", "aproveitamento");
-      formData.append("course_workload", courseWorkload);
-      // Para 'aproveitamento', você deve adicionar o que for relevante.
-    } else {
-      console.error("Tipo de requisição inválido.");
-      return; // Para o envio se o tipo de requisição não for válido
-    }
+        if (requestType === "certificacao") {
+            formData.append("requestType", "certificacao");
+            formData.append("previous_knowledge", previousKnowledge);
+        } else if (requestType === "aproveitamento") {
+            formData.append("requestType", "aproveitamento");
+        } else {
+            console.error("Tipo de requisição inválido.");
+            return; // Para o envio se o tipo de requisição não for válido
+        }
 
-    const endpoint = requestType === "certificacao"
-      ? baseURL + "/forms/knowledge-certifications/"
-      : baseURL + "/forms/recognition-forms/";
+        const endpoint = requestType === "certificacao"
+            ? baseURL + "/forms/knowledge-certifications/"
+            : baseURL + "/forms/recognition-forms/";
 
-    try {
-      const response = await fetch(endpoint, {
-        method: "POST",
-        body: formData,
-      });
+        try {
+            const response = await fetch(endpoint, {
+                method: "POST",
+                body: formData,
+            });
 
-      if (response.ok) {
-        console.log("Formulário enviado com sucesso!");
-      } else {
-        const errorData = await response.json();
-        console.error("Erro ao enviar o formulário:", errorData);
-      }
-    } catch (error) {
-      console.error("Erro ao enviar o formulário:", error);
-    }
-  };
+            if (response.ok) {
+                console.log("Formulário enviado com sucesso!");
+            } else {
+                const errorData = await response.json();
+                console.error("Erro ao enviar o formulário:", errorData);
+            }
+        } catch (error) {
+            console.error("Erro ao enviar o formulário:", error);
+        }
+    };
 
-  const handleCancel = () => {
-    console.log("teste cancelar");
-  };
+    const handleCancel = () => {
+        console.log("teste cancelar");
+    };
 
-  return (
-    <form className={styles.formContainer} onSubmit={handleSubmit}>
-      <div className={styles.typeContainer}>
-        <label style={{ fontWeight: "700", fontSize: "20px" }}>Tipo</label>
-        <select
-          value={requestType}
-          onChange={(e) => setRequestType(e.target.value)}
-          className={styles.typeSelect}
-        >
-          <option value="">Selecione</option>
-          <option value="certificacao">Certificação de Conhecimento</option>
-          <option value="aproveitamento">Aproveitamento de Estudos</option>
-        </select>
-      </div>
-      <div className={styles.typeContainer}>
-        <label style={{ fontWeight: "700", fontSize: "20px" }}>Curso</label>
-        <select
-          value={selectedCourse}
-          onChange={handleCourseChange}
-          className={styles.typeSelect}
-          required
-        >
-          <option value="">Selecione um curso</option>
-          {courses.map((course) => (
-            <option key={course.id} value={course.id}>
-              {course.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className={styles.typeContainer}>
-        <label style={{ fontWeight: "700", fontSize: "20px" }}>Disciplina</label>
-        <select
-          value={disciplineId}
-          onChange={(e) => setDisciplineId(e.target.value)}
-          className={styles.typeSelect}
-          disabled={!selectedCourse} // Desabilita se nenhum curso estiver selecionado
-          required
-        >
-          <option value="">Selecione uma disciplina</option>
-          {disciplines.map((discipline) => (
-            <option key={discipline.id} value={discipline.id}>
-              {discipline.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      {requestType === "certificacao" && (
-        <div className={styles.typeContainer}>
-          <label style={{ fontWeight: "700", fontSize: "20px" }}>Conhecimento Anterior</label>
-          <textarea
-            value={previousKnowledge}
-            onChange={(e) => setPreviousKnowledge(e.target.value)}
-            placeholder="Descreva seu conhecimento anterior..."
-            className={styles.typeSelect}
-            required
-          />
-        </div>
-      )}
-      {requestType === "aproveitamento" && (
-        <div className={styles.typeContainer}>
-          <label style={{ fontWeight: "700", fontSize: "20px" }}>Carga Horária</label>
-          <input
-            type="number"
-            value={courseWorkload}
-            onChange={(e) => setCourseWorkload(e.target.value)}
-            placeholder="Carga horária em horas"
-            className={styles.typeSelect}
-            required
-          />
-        </div>
-      )}
-      {/* Anexos removidos */}
-      <div className={styles.formBtnContainer}>
-        <Button type={"button"} color={"#af0a0a"} onClick={handleCancel}>
-          Cancelar
-        </Button>
-        <Button type={"submit"} style={{ marginLeft: 'auto' }}>Enviar</Button>
-      </div>
-    </form>
-  );
+    return (
+        <form className={styles.formContainer} onSubmit={handleSubmit}>
+            <div className={styles.typeContainer}>
+                <label style={{fontWeight: "700", fontSize: "20px"}}>Tipo</label>
+                <select
+                    value={requestType}
+                    onChange={(e) => setRequestType(e.target.value)}
+                    className={styles.typeSelect}
+                >
+                    <option value="">Selecione</option>
+                    <option value="certificacao">Certificação de Conhecimento</option>
+                    <option value="aproveitamento">Aproveitamento de Estudos</option>
+                </select>
+            </div>
+            <div className={styles.typeContainer}>
+                <label style={{fontWeight: "700", fontSize: "20px"}}>Curso</label>
+                <select
+                    value={selectedCourse}
+                    onChange={handleCourseChange}
+                    className={styles.typeSelect}
+                    required
+                >
+                    <option value="">Selecione um curso</option>
+                    {courses.map((course) => (
+                        <option key={course.id} value={course.id}>
+                            {course.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className={styles.typeContainer}>
+                <label style={{fontWeight: "700", fontSize: "20px"}}>Disciplina</label>
+                <select
+                    value={disciplineId}
+                    onChange={(e) => setDisciplineId(e.target.value)}
+                    className={styles.typeSelect}
+                    disabled={!selectedCourse} // Desabilita se nenhum curso estiver selecionado
+                    required
+                >
+                    <option value="">Selecione uma disciplina</option>
+                    {disciplines.map((discipline) => (
+                        <option key={discipline.id} value={discipline.id}>
+                            {discipline.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            {requestType === "certificacao" && (
+                <div className={styles.typeContainer}>
+                    <label style={{fontWeight: "700", fontSize: "20px"}}>Conhecimento Anterior</label>
+                    <textarea
+                        value={previousKnowledge}
+                        onChange={(e) => setPreviousKnowledge(e.target.value)}
+                        placeholder="Descreva seu conhecimento anterior..."
+                        className={styles.typeSelect}
+                        required
+                    />
+                </div>
+            )}
+            {requestType === "aproveitamento" && (
+                <div className={styles.typeContainer}>
+                    <label style={{fontWeight: "700", fontSize: "20px"}}>Carga Horária</label>
+                    <input
+                        type="number"
+                        value={courseWorkload}
+                        onChange={(e) => setCourseWorkload(e.target.value)}
+                        placeholder="Carga horária em horas"
+                        className={styles.typeSelect}
+                        required
+                    />
+                    <label style={{fontWeight: "700", fontSize: "20px"}}>Carga Horária Estudada</label>
+                    <input
+                        type="number"
+                        value={courseStudiedWorkload}
+                        onChange={(e) => setCourseStudiedWorkload(e.target.value)}
+                        placeholder="Carga horária estudada em horas"
+                        className={styles.typeSelect}
+                        required
+                    />
+                </div>
+            )}
+            <div className={styles.formBtnContainer}>
+                <Button type={"button"} color={"#af0a0a"} onClick={handleCancel}>
+                    Cancelar
+                </Button>
+                <Button type={"submit"} style={{marginLeft: 'auto'}}>Enviar</Button>
+            </div>
+        </form>
+    );
 };
 
 export default CertificationRequestForm;
