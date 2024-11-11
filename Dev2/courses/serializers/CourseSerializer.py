3d853bdb300c9709dfe7ca609f8c820b5095c82f
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-11-10 21:09:30 -0300

    fixes_serializers

--- Dev2/courses/serializers/CourseSerializer.py
+++ Dev2/courses/serializers/CourseSerializer.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 
 from ..models import Course
-from disciplines.serializers import DisciplinesSerializer
+from disciplines.models import Disciplines
 
-from users.serializers import ServantSerializer
+from users.models import Servant
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    professors = ServantSerializer(many=True)
-    disciplines = DisciplinesSerializer(many=True)
+    professors = serializers.PrimaryKeyRelatedField(queryset=Servant.objects.all(), many=True)
+    disciplines = serializers.PrimaryKeyRelatedField(queryset=Disciplines.objects.all(), many=True)
 
     class Meta:
         model = Course
