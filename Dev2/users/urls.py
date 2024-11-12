87e7f8c34b8c37ad49936e054dd611970fea53af
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-11-11 21:59:05 -0300

    desisssstoooooo_edit_course

--- Dev2/users/urls.py
+++ Dev2/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views.user_list_view import ListUsersAPIView, ListUserAPIView
+from .views.user_list_view import ListUsersAPIView
 from .views.user import CreateUserView, AlterActiveByEmailView,RetrieveUserByIdAPIView
 from .views.user_details import UserDetails
 
 urlpatterns = [
     path('list/', ListUsersAPIView.as_view(), name='list_users'),
-    path('lists/', ListUserAPIView.as_view(), name='lists_users'),
     path('create/', CreateUserView.as_view(), name='create_user'),
     path('alter-activity/<str:email>/', AlterActiveByEmailView.as_view(), name='create_user'),
     path('details/', UserDetails.as_view(), name='user-details'),
--- Dev2/users/views/user_list_view.py
+++ Dev2/users/views/user_list_view.py
@@ -4,7 +4,7 @@ from ..models.student import Student
 from ..models.servant import Servant
 from api.views.custom_api_view import CustomAPIView
 from users.models.user import AbstractUser
-from ..serializers.user import UserPolymorphicSerializer, UserSerializer
+from ..serializers.user import UserPolymorphicSerializer
 from django.db.models import Q
 
 
@@ -37,23 +37,3 @@ class ListUsersAPIView(APIView):
 
         serializer = UserPolymorphicSerializer(queryset, many=True)
         return Response(serializer.data)
-
-
-class ListUserAPIView(APIView):
-
-    def get(self, request, *args, **kwargs):
-        user_name = request.GET.get('user_name')
-
-        users_filter = Q()
-
-        # Filtro pelo nome da disciplina
-        if user_name:
-            users_filter &= Q(name__icontains=user_name)
-
-        # Buscando disciplinas de acordo com os filtros aplicados
-        users = AbstractUser.objects.filter(users_filter)
-
-        # Serializando os resultados
-        users_serialized = UserSerializer(users, many=True)
-
-        return Response({'disciplines': users_serialized.data})
