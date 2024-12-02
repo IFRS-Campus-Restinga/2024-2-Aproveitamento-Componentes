879ee403823bafefbff26470be2e85859aac58c5
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-12-02 06:28:41 -0300

    validacao para apenas coord e ensino criar, atualizar ou deletar disciplinas

--- Dev2/disciplines/views.py
+++ Dev2/disciplines/views.py
@@ -4,17 +4,25 @@ from rest_framework import status
 from .models import Disciplines
 from .serializers import DisciplineSerializer
 from rest_framework.permissions import IsAuthenticated
+from users.services.user import UserService
 
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def discipline_list_create(request):
+    user_service = UserService()
+
     if request.method == 'GET':
         disciplines = Disciplines.objects.all()
         serializer = DisciplineSerializer(disciplines, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
+        usuario = request.user
+        if not user_service.userAutorized(usuario):
+            return Response({"detail": "Usuário não autorizado para criar disciplinas."},
+                            status=status.HTTP_403_FORBIDDEN)
+
         serializer = DisciplineSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -24,6 +32,8 @@ def discipline_list_create(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated])
 def discipline_detail(request, pk):
+    user_service = UserService()
+
     try:
         discipline = Disciplines.objects.get(pk=pk)
     except Disciplines.DoesNotExist:
@@ -34,6 +44,12 @@ def discipline_detail(request, pk):
     if request.method == 'GET':
         serializer = DisciplineSerializer(discipline)
         return Response(serializer.data)
+
+    # validação
+    usuario = request.user
+    if not user_service.userAutorized(usuario):
+        return Response({"detail": "Usuário não autorizado."},
+                        status=status.HTTP_403_FORBIDDEN)
 
     elif request.method == 'PUT':
         print("Método PUT chamado")  # Log de teste
