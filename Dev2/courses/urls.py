90af62199a4eb35aea061d94435ea50b2453f71d
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-12-16 21:04:11 -0300

    inserida url

--- Dev2/courses/urls.py
+++ Dev2/courses/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views.course_view import (ListCoursesAPIView, CreateCourseAPIView,
                                 RetrieveCourseByIdAPIView, UpdateCourseAPIView,
                                 DeleteCourseAPIView, SearchCourseByNameAPIView, CourseProfessorsView)
+from .views import plano_view
 
 urlpatterns = [
     path('list', ListCoursesAPIView.as_view(), name='list_courses'),
@@ -11,5 +12,7 @@ urlpatterns = [
     path('update/<uuid:course_id>', UpdateCourseAPIView.as_view(), name='update_course'),
     path('delete/<uuid:course_id>', DeleteCourseAPIView.as_view(), name='delete_course'),
     path('search/', SearchCourseByNameAPIView.as_view(), name='search_course_by_name'),
-    path('professors/<int:coordinator_id>', CourseProfessorsView.as_view(), name='professors')
+    path('professors/<int:coordinator_id>', CourseProfessorsView.as_view(), name='professors'),
+    path('plano/list', plano_view.plano_list_create, name='plano_list_create'),
+    path('plano/<int:id>/', plano_view.plano_detail, name='plano_detail'),
 ]
